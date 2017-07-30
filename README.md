cf32f403abf59126f3c491ab58ee452af73e9db4
Yasmin Habayeb <yasmin@Yasmins-MBP.lan>
2017-07-30 20:52:40 +0200

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+This is a simple Vue.js project so a workshop could understand the basics of Vue.js and expand on the functionalities themselves.
+
+
+``` bash
+# install dependencies
+npm install
+
+# serve with hot reload at localhost:8080
+npm run dev
+```
---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app" class="app">
+    <h1 v-bind:class="{ warning: total >= 3 }">{{ total }}</h1>
+    <div v-on:click="increment" class="btn">button</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      total: 0
+    }
+  },
+  methods: {
+    increment: function () {
+      this.total++;
+    }
+  }
+}
+</script>
+
+<style>
+.app {
+  padding: 0 25px;
+}
+
+.warning {
+  color: red;
+  font-weight: bold;
+}
+</style>
